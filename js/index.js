d4810a50688b269cba1e2f2f170a2b82e4e72558
Christopher Vazquez <c_vazquez08@yahoo.com>
2018-12-01 18:08:18 -0500

    refactored + added movement

--- js/index.js
+++ js/index.js
@@ -1,78 +1,91 @@
-//  target <canvas> by ID "snake-board"
-const myCanvas = document.getElementById("snake-board");
-const ctx = myCanvas.getContext("2d");
+// const: Colors for background / border
+// colors for snake and snake-border
+const canvasBorderColor = 'black';
+const canvasBackgroundColor  = "lightblue";
+const snakeColor = "lightgreen";
+const snakeBorderColor = "darkgreen";
 
-//  snake object
-//  with x and y starting points 
-//  snake size
-//  && moving functions
-let snake = {
-  x: 500,
-  y: 250,
-  width: 25,
-  height: 25,
-  moveUp:    function() { this.y -= 10 },
-  moveDown:  function() { this.y += 10 },
-  moveLeft:  function() { this.x -= 10 },
-  moveRight: function() { this.x += 10 },
+
+// x && y coordinates for snake
+// each "square" has it's down x && y axis
+let snake = [
+  {x: 150, y: 150},
+  {x: 140, y: 150},
+  {x: 130, y: 150},
+  {x: 120, y: 150},
+  {x: 110, y: 150}
+]
+
+
+// get the canvas element
+let gameCanvas = document.getElementById("snake-board");
+// get the ctx 
+let ctx = gameCanvas.getContext("2d");
+
+function drawBackground(){
+// set / draw canvas background color
+ctx.fillStyle = canvasBackgroundColor;
+ctx.fillRect(0, 0, 1000, 500);
+//set / draw canvas border color
+ctx.strokestyle = canvasBorderColor;
+ctx.strokeRect(0, 0, 1000, 500);
 }
 
- // function that draws snake
-function draw(snake){
-  ctx.fillRect(snake.x, snake.y, snake.width, snake.height);
+// loops through each snakePart and calls drawSnakePart
+function drawSnake() {
+  snake.forEach(drawSnakePart)
 }
 
-// apple object
-//  x and y starting points
-//  width and height set
-let apple = {
-  x: 300,
-  y: 50,
-  width: 10,
-  height: 10,
- 
-}
 
-// function that draws apple
-function draw(apple){
-  ctx.fillRect(apple.x, apple.y, apple.width, apple.height);
+function drawSnakePart(snakePart){
+  //  set color / draw snakePart
+  ctx.fillStyle = snakeColor;
+  ctx.fillRect(snakePart.x, snakePart.y, 10, 10);
+  //  set border / draw border for snakePart
+  ctx.strokestyle = snakeBorderColor;
+  ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
+
 }
 
 function updateCanvas(){
   ctx.clearRect(0, 0, 1000, 500);
-  draw(snake);
-  draw(apple);
- 
+  drawBackground();
+  drawSnake();
 }
 
-// switch for movement keys
+
+
 document.onkeydown = function(event) {
+
+  let head;
+
   switch (event.keyCode) {
     case 38:  //38:upArrow 87:W
     case 87: 
-      snake.moveUp();  
+    head = {x: snake[0].x, y: snake[0].y -10};
       break; 
     case 40:  //40:downArrow 83:S
     case 83: 
-      snake.moveDown(); 
+    head = {x: snake[0].x, y: snake[0].y + 10};
       break; 
     case 37:  //37:leftArrow: 65:A
     case 65: 
-      snake.moveLeft(); 
+    head = {x: snake[0].x - 10, y: snake[0].y};
       break; 
     case 39:  //39:rightArrow 68:D
     case 68: 
-      snake.moveRight();
+    head = {x: snake[0].x + 10, y: snake[0].y};        
       break; 
   }
- // updates canvas on every move
+  // updates canvas on every move
+  advance(head)
   updateCanvas();
 }
 
-function collision(){
-  
+function advance(head){
+  snake.unshift(head);
+  snake.pop();
 }
 
-// intial update with intial values
-updateCanvas();
 
+  updateCanvas();
