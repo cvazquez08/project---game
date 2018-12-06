90f3971bf301a0cc9c30072c37795f76d841c917
Christopher Vazquez <c_vazquez08@yahoo.com>
2018-12-05 21:28:41 -0500

    added ability for snake to eat food and some styling + styled html

--- js/index.js
+++ js/index.js
@@ -3,7 +3,9 @@
 const canvasBorderColor = 'black';
 const canvasBackgroundColor  = "black";
 const snakeColor = "#20FF00";
-const snakeBorderColor = "#20FF00";
+const snakeBorderColor = "black";
+const foodColor = "red";
+const foodBorderColor = "black";
 // target width and height and set to variable for easy targeting
 const width = document.getElementById("snake-board").width;
 const height = document.getElementById("snake-board").height;
@@ -17,23 +19,29 @@ const cellWidth = 10;
 let direction;
 let score;
 let snakeArray;
-
+let game_loop;
 
 // the game function which calls snake / food function in a loop with set interval
 function play(){
   direction = "right" // setting default direction for constant moving
   snake();
-  food();
+  randomFood();
 
   score = 0;
 
   if (typeof game_loop != "undefined") clearInterval(game_loop);
-  game_loop = setInterval(paint, 60);
+  // maybe useless line = > can use to stop game.
+  game_loop = setInterval(paint, 50);
 
   
 }
 
-play();
+// on click for play button
+const playBtn = document.getElementsByClassName("start-game")[0];
+
+playBtn.onclick = function(){
+  play();
+}
 
 // snake function assigns all objects into array
 function snake(){
@@ -46,7 +54,7 @@ function snake(){
 
 }
 
-function food(){
+function randomFood(){
   food = {
     // randomize an X and Y axis between the board
     x: Math.round(Math.random() * (width - cellWidth) / cellWidth),
@@ -74,24 +82,38 @@ function paint(){
   let headY = snakeArray[0].y;
 
   // add movement
-  if (direction == "right") headX++;
-  else if(direction == "left") headX--;
-  else if(direction == "up") headY--;
-  else if(direction == "down") headY++;
+  if (direction === "right") headX++;
+  else if(direction === "left") headX--;
+  else if(direction === "up") headY--;
+  else if(direction === "down") headY++;
 
-// assigns last object in array to tail variable
+  if(headX == food.x && headY == food.y){
+
+    let tail = {x: headX, y: headY};
+    snakeArray.unshift(tail);
+    score++;
+    randomFood();
+  }else {
+  
+    // if not assign last object in array to tail variable
   let tail = snakeArray.pop();
   tail.x = headX; // reasigns new tail to headX 
   tail.y = headY; // headY => so snake can keep moving
   snakeArray.unshift(tail); //unshifts tail to front of array and keeps looping
   // paint loops every 60ms from the play();
 
+  }
+
+  
+
   // paint food = > already have it with random coordinates
-  paintCell(food.x, food.y);
+  paintFood(food.x, food.y);
 
   // add score
   let scoreText = "Score: " + score;
-  ctx.fillText(scoreText, 20, 410, 50, 50);
+  ctx.font = "20px VT323";
+  ctx.fillStyle = snakeColor;
+  ctx.fillText(scoreText, 20, 490);
 
 }
 
@@ -99,33 +121,50 @@ function paint(){
 function paintCell(x, y) {
   ctx.fillStyle = snakeColor;
   ctx.fillRect(x * cellWidth, y * cellWidth, cellWidth, cellWidth);
-  ctx.strokeStyle = snakeBorderColor
+  ctx.strokeStyle = snakeBorderColor;
   ctx.strokeRect(x * cellWidth, y * cellWidth, cellWidth, cellWidth);
 }
 
+function paintFood(x, y){
+  ctx.fillStyle = foodColor;
+  ctx.fillRect(x * cellWidth, y * cellWidth, cellWidth, cellWidth);
+  ctx.strokeStyle = foodBorderColor;  
+  ctx.strokeRect(x * cellWidth, y * cellWidth, cellWidth, cellWidth);  
+
+}
 // event listner when key is pressed
 document.onkeydown = function(event) {
   switch (event.keyCode) {
     case 38:  //38:upArrow 87:W
     case 87: 
+    if(direction != "down"){
     direction = "up";
+    }
       break; 
 
     case 40:  //40:downArrow 83:S
     case 83: 
+    if(direction != "up"){
     direction = "down";
+    }
       break; 
 
     case 37:  //37:leftArrow: 65:A
     case 65: 
+    if(direction != "right"){
     direction = "left";
+    }
       break; 
 
     case 39:  //39:rightArrow 68:D
     case 68: 
-    direction = "right";     
+    if(direction != "left"){
+    direction = "right";    
+    } 
       break; 
   }
+
+  console.log(event.keyCode);
 }
 
 
